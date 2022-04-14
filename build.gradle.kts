457992e1292fa885f54b67a481120aecf68ebaee
Cody Worsnop <cworsnop@figure.com>
2022-04-14 07:56:37 -0700

    githubReleases + semversioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import net.swiftzer.semver.SemVer
+
+buildscript {
+    dependencies {
+        classpath("net.swiftzer.semver:semver:1.1.2")
+        classpath("com.github.breadmoirai:github-release:2.2.12")
+    }
+}
+
 plugins {
     kotlin("jvm") version Versions.Kotlin
     id("java")
     id("maven-publish")
-}
-
-allprojects {
-    val project = this
-    group = "io.provenance.p8e-cee-api"
-    version = this.findProperty("artifactVersion")?.toString()
-        ?: "1.0-snapshot"
-
-    repositories {
-        mavenCentral()
-    }
+    id("com.github.breadmoirai.github-release") version "2.2.12"
+    id("io.github.nefilim.gradle.semver-plugin") version "0.3.10"
 }
 
 subprojects {
@@ -44,4 +45,85 @@ subprojects {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+semver {
+    tagPrefix("v")
+    initialVersion("0.1.0")
+    findProperty("semver.overrideVersion")?.toString()?.let { overrideVersion(it) }
+    val semVerModifier = findProperty("semver.modifier")?.toString()?.let { buildVersionModifier(it) } ?: { nextPatch() }
+    versionModifier(semVerModifier)
+}
+
+/**
+ * The code below is a workaround for [gradle/gradle#20016](https://github.com/gradle/gradle/issues/20016) derived from
+ * [platform-portal@14319ed/docs/semver.md](https://github.com/FigureTechnologies/platform-portal/blob/14319ed7e97d88a1b8cbb2f2f7708cc0660dc518/docs/semver.md#limiting-dependencies-to-release-versions)
+ * to prevent version strings like `"1.0.+"` from resolving to pre-release versions.
+ * You can adjust the string set constants or comment the code out entirely to allow pre-release versions to be used.
+ */
+
+val invalidQualifiers = setOf("alpha", "beta", "rc", "nightly")
+val onlyReleaseArtifacts = setOf("p8e-cee-api")
+val whiteListedMavenGroups = setOf("tech.figure", "io.provenance")
+
+configurations.all {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                when {
+                    (
+                        onlyReleaseArtifacts.any { candidate.moduleIdentifier.name.startsWith(it) } &&
+                            !candidate.version.toSemVer()?.preRelease.isNullOrEmpty()
+                        ) -> {
+                        reject("Rejecting prerelease version for OnlyReleaseArtifact[$candidate]")
+                    }
+                    (
+                        whiteListedMavenGroups.none { candidate.group.startsWith(it) } &&
+                            invalidQualifiers.any { candidate.version.contains(it) }
+                        ) -> {
+                        reject("Invalid qualifier versions for $candidate")
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun String?.toSemVer(): SemVer? =
+    try {
+        this?.let { versionString ->
+            SemVer.parse(versionString)
+        }
+    } catch (e: Exception) {
+        project.logger.info("Failed to parse semantic version from string '$this'")
+        null
+    }
+
+
+val semVersion = semver.version
+allprojects {
+    val project = this
+    group = "io.provenance.p8e-cee-api"
+    version = semVersion
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+val githubTokenValue = findProperty("githubToken")?.toString() ?: System.getenv("GITHUB_TOKEN")
+githubRelease {
+    token(githubTokenValue)
+    owner("provenance-io")
+    targetCommitish("main")
+    draft(false)
+    prerelease(false)
+    repo("p8e-cee-api")
+    tagName(semver.versionTagName)
+    body(changelog())
+
+    overwrite(false)
+    dryRun(false)
+    apiEndpoint("https://api.github.com")
+    client
 }
