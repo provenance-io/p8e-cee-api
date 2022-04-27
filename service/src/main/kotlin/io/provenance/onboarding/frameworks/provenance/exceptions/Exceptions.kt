8342e5fa32d145418033dc8c5fd8556189503cfb
Cody Worsnop <cworsnop@figure.com>
2022-04-27 15:04:53 -0700

    lint

--- service/src/main/kotlin/io/provenance/onboarding/frameworks/provenance/exceptions/Exceptions.kt
+++ service/src/main/kotlin/io/provenance/onboarding/frameworks/provenance/exceptions/Exceptions.kt
@@ -1,3 +1,9 @@
 package io.provenance.onboarding.frameworks.provenance.exceptions
 
 open class ContractExceptionException(message: String? = "", val shouldRetry: Boolean = false, cause: Throwable? = null) : RuntimeException(message, cause)
+
+open class ResourceNotFoundException(message: String? = "", cause: Throwable? = null) : RuntimeException(message, cause)
+
+open class WontRunLoanValidationException(message: String? = "", cause: Throwable? = null) : RuntimeException(message, cause)
+
+open class ExistingScopeNotChangedException(message: String) : java.lang.RuntimeException(message)
