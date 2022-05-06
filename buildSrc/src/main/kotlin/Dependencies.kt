633ff73e90b0b6574f53a04a78ca6d802e3458e7
Cody Worsnop <cworsnop@figure.com>
2022-05-06 12:19:17 -0700

    lmao it didnt check in the build file

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -15,13 +15,11 @@ object Versions {
     const val Kotlin = "1.5.31"
     const val KotlinCoroutines = "1.5.2"
     const val Protobuf = "3.18.1"
-    const val Kafka = "2.7.0"
     const val SpringBoot = "2.5.6"
     const val KotlinLogging = "2.0.11"
     const val Reactor = "3.4.9"
     const val Jackson = "2.12.5"
     const val Redisson = "3.16.0"
-    const val Flyaway = "8.0.2"
     const val Ktlint = "0.42.1"
     const val Detekt = "1.18.1"
     const val Hamkrest = "1.8.0.1"
@@ -51,11 +49,7 @@ object Plugins { // please keep this sorted in sections
 
     // 3rd Party
     val Detekt = PluginSpec("io.gitlab.arturbosch.detekt", Versions.Detekt)
-    val Flyway = PluginSpec("org.flywaydb.flyway", Versions.Flyaway)
     val Idea = PluginSpec("idea")
-    val MavenPublish = PluginSpec("maven-publish")
-    val JavaLibrary = PluginSpec("java-library")
-    val Signing = PluginSpec("signing")
     val Protobuf = PluginSpec("com.google.protobuf", "0.8.16")
     val SpringBoot = PluginSpec("org.springframework.boot", Versions.SpringBoot)
     val SpringDependencyManagement = PluginSpec("io.spring.dependency-management", "1.0.11.RELEASE")
@@ -92,6 +86,8 @@ object Dependencies {
         val OsClient = DependencySpec("io.provenance.scope:os-client", Versions.P8eScope)
         val Sdk = DependencySpec("io.provenance.scope:sdk", Versions.P8eScope)
         val Util = DependencySpec("io.provenance.scope:util", Versions.P8eScope)
+        val ContractBase = DependencySpec("io.provenance.scope:contract-base", Versions.P8eScope)
+        val ContractProto = DependencySpec("io.provenance.scope:contract-proto", Versions.P8eScope)
     }
 
     // Spring Boot
@@ -131,11 +127,6 @@ object Dependencies {
         val JavaUtil = DependencySpec("com.google.protobuf:protobuf-java-util", Versions.Protobuf)
     }
 
-    // Apache Kafka
-    object Kafka {
-        val Clients = DependencySpec("org.apache.kafka:kafka-clients", Versions.Kafka)
-    }
-
     object Kong {
         val Unirest = DependencySpec("com.konghq:unirest-java", Versions.Unirest)
     }
@@ -161,19 +152,7 @@ object Dependencies {
 
     object Provenance {
         val KeyAccessLib = DependencySpec("io.provenance.originator-key-access-lib:lib", Versions.KeyAccessLib)
-        val CoreKcache = DependencySpec("io.provenance:core-kcache", Versions.ProvenanceCore)
-        val CoreKcacheSpring = DependencySpec("io.provenance:core-kcache-spring", Versions.ProvenanceCore)
-        val CoreKafkaAggregator = DependencySpec("io.provenance:core-kafka-aggregator", Versions.ProvenanceCore)
         val ProtoKotlin = DependencySpec("io.provenance:proto-kotlin", Versions.ProvenanceProto)
-        val CoreKafkaAggregatorSpring =
-            DependencySpec(
-                "io.provenance:core-kafka-aggregator-spring",
-                Versions.ProvenanceCore,
-                exclude = listOf(
-                    "org.springframework.boot:spring-boot-starter-web",
-                    "org.springframework.boot:spring-boot-starter-security"
-                )
-            )
         val AssetModel = DependencySpec("io.provenance.model:metadata-asset-model", Versions.AssetModel)
         val LoanPackage = DependencySpec("io.provenance.loan-package:contract", Versions.LoanPackage)
 
---
+++ models/build.gradle.kts
@@ -0,0 +1,98 @@
+val ktlint: Configuration by configurations.creating
+
+plugins {
+    Plugins.Detekt.addTo(this)
+    `java-library`
+    `maven-publish`
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+dependencies {
+    ktlint(Dependencies.Ktlint.toDependencyNotation())
+
+    listOf(
+        Dependencies.Protobuf.JavaUtil,
+        Dependencies.P8eScope.ContractProto,
+        Dependencies.P8eScope.ContractBase,
+        Dependencies.Provenance.AssetModel,
+        Dependencies.Kotlin.CoroutinesReactor,
+    ).forEach { dep ->
+        dep.implementation(this)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "io.provenance.p8e-cee-api"
+            artifactId = project.name
+
+            from(components["java"])
+
+            pom {
+                name.set("Provenance p8e-cee-api data classes")
+                description.set("Provenance p8e-cee-api data classes")
+                url.set("https://provenance.io")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("cworsnop-figure")
+                        name.set("Cody Worsnop")
+                        email.set("cworsnop@figure.com")
+                    }
+                }
+
+                scm {
+                    connection.set("git@github.com:provenance-io/p8e-cee-api.git")
+                    developerConnection.set("git@github.com:provenance-io/p8e-cee-api.git")
+                    url.set("https://github.com/provenance-io/p8e-cee-api")
+                }
+            }
+        }
+    }
+
+    signing {
+        sign(publishing.publications["maven"])
+    }
+
+    tasks.javadoc {
+        if(JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+}
+
+tasks.register<JavaExec>("ktlint") {
+    group = "verification"
+    description = "Check Kotlin code style."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args("src/**/*.kt")
+}
+
+tasks.named("check") {
+    dependsOn(tasks.named("ktlint"))
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    group = "formatting"
+    description = "Fix Kotlin code style deviations."
+    classpath = ktlint
+    main = "com.pinterest.ktlint.Main"
+    args("-F", "src/**/*.kt")
+}
+
+detekt {
+    toolVersion = Versions.Detekt
+    buildUponDefaultConfig = true
+    config = files("${rootDir.path}/detekt.yml")
+    input = files("src/main/kotlin", "src/test/kotlin")
+}
