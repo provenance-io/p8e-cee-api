9200b5184e0bd8a2dcd81ac83ef5fc9d90796042
Cody Worsnop <cworsnop@figure.com>
2022-07-13 12:03:51 -0700

    file updates (#44)

--- service/src/main/kotlin/io/provenance/api/domain/usecase/objectStore/get/RetrieveAndDecrypt.kt
+++ service/src/main/kotlin/io/provenance/api/domain/usecase/objectStore/get/RetrieveAndDecrypt.kt
@@ -4,15 +4,14 @@ import io.provenance.api.domain.objectStore.ObjectStore
 import io.provenance.api.domain.usecase.AbstractUseCase
 import io.provenance.api.domain.usecase.common.originator.EntityManager
 import io.provenance.api.domain.usecase.common.originator.models.KeyManagementConfigWrapper
-import io.provenance.api.frameworks.config.ObjectStoreConfig
 import io.provenance.api.domain.usecase.objectStore.get.models.RetrieveAndDecryptRequest
+import io.provenance.api.frameworks.config.ObjectStoreConfig
 import io.provenance.scope.objectstore.client.OsClient
-import java.lang.IllegalStateException
 import java.net.URI
 import java.security.PrivateKey
 import java.security.PublicKey
+import org.apache.commons.codec.binary.Base64
 import org.springframework.stereotype.Component
-import java.util.Base64
 
 @Component
 class RetrieveAndDecrypt(
@@ -29,7 +28,12 @@ class RetrieveAndDecrypt(
             ?: throw IllegalStateException("Private key was not present for originator: ${args.uuid}")
 
         OsClient(URI.create(args.objectStoreAddress), objectStoreConfig.timeoutMs).use { osClient ->
-            return objectStore.retrieveAndDecrypt(osClient, Base64.getUrlDecoder().decode(args.hash), publicKey, privateKey)
+            return objectStore.retrieveAndDecrypt(osClient, decodeBase64(args.hash), publicKey, privateKey)
         }
     }
+
+    private fun decodeBase64(string: String): ByteArray =
+        string.replace(' ', '+').let {
+            Base64.decodeBase64(it)
+        }
 }
