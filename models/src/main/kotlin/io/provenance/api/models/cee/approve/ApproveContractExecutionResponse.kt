e7548589aa657088161bfed98295ee32cba1e453
Cody Worsnop <cworsnop@figure.com>
2022-07-14 08:30:11 -0700

    Returning state from approval (#46)
    * Returning state from approval
    
    * name change
    
    * name change

---
+++ models/src/main/kotlin/io/provenance/api/models/cee/approve/ApproveContractExecutionResponse.kt
@@ -0,0 +1,8 @@
+package io.provenance.api.models.cee.approve
+
+import io.provenance.api.models.p8e.TxResponse
+
+data class ApproveContractExecutionResponse(
+    val envelopeStateBase64EncodedByteArray: String,
+    val tx: TxResponse
+)
