b5671773cbfe857002386ae419937202137be7a1
Cody Worsnop <cworsnop@figure.com>
2022-08-02 16:34:31 -0700

    Scope Query Response (#52)

--- service/src/main/kotlin/io/provenance/api/domain/usecase/provenance/query/QueryScope.kt
+++ service/src/main/kotlin/io/provenance/api/domain/usecase/provenance/query/QueryScope.kt
@@ -4,13 +4,16 @@ import io.provenance.api.domain.usecase.AbstractUseCase
 import io.provenance.api.frameworks.provenance.ProvenanceService
 import io.provenance.api.models.p8e.ProvenanceConfig
 import io.provenance.api.models.p8e.query.QueryScopeRequest
-import io.provenance.metadata.v1.ScopeResponse
+import io.provenance.api.models.p8e.query.QueryScopeResponse
+import java.util.Base64
 import org.springframework.stereotype.Component
 
 @Component
 class QueryScope(
     private val provenanceService: ProvenanceService,
-) : AbstractUseCase<QueryScopeRequest, ScopeResponse>() {
-    override suspend fun execute(args: QueryScopeRequest): ScopeResponse =
-        provenanceService.getScope(ProvenanceConfig(args.chainId, args.nodeEndpoint), args.scopeUuid, args.height)
+) : AbstractUseCase<QueryScopeRequest, QueryScopeResponse>() {
+    override suspend fun execute(args: QueryScopeRequest): QueryScopeResponse =
+        QueryScopeResponse(
+            Base64.getEncoder().encodeToString(provenanceService.getScope(ProvenanceConfig(args.chainId, args.nodeEndpoint), args.scopeUuid, args.height).toByteArray())
+        )
 }
