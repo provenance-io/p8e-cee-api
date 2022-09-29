b8c311664e9387710bd82b3fb35e8110862bc773
Cody Worsnop <cworsnop@figure.com>
2022-09-29 09:55:32 -0700

    File Part Byte issue

--- service/src/main/kotlin/io/provenance/api/util/MultiPartExtensions.kt
+++ service/src/main/kotlin/io/provenance/api/util/MultiPartExtensions.kt
@@ -1,13 +1,8 @@
 package io.provenance.api.util
 
 import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.codec.multipart.FilePart
-import java.io.ByteArrayOutputStream
 
-suspend fun FilePart.awaitAllBytes(): ByteArray = ByteArrayOutputStream().use { stream ->
-    /* read all data frames from the request, then concat together via the output stream */
-    content().collectList().awaitSingle().forEach { buffer ->
-        stream.writeBytes(buffer.asByteBuffer().array())
-    }
-    stream.toByteArray()
-}
+suspend fun FilePart.awaitAllBytes(): ByteArray =
+    DataBufferUtils.join(this.content()).map { it.asByteBuffer().array() }.awaitSingle()
