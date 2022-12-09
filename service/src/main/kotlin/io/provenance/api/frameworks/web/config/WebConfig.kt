40f463a89b41215adb42b190420351cb7469ee44
Cody Worsnop <cworsnop@figure.com>
2022-12-09 10:47:45 -0800

    max file size changes (#74)

--- service/src/main/kotlin/io/provenance/api/frameworks/web/config/WebConfig.kt
+++ service/src/main/kotlin/io/provenance/api/frameworks/web/config/WebConfig.kt
@@ -4,17 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.http.codec.multipart.DefaultPartHttpMessageReader
+import org.springframework.http.codec.multipart.MultipartHttpMessageReader
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 @EnableWebFlux
 class WebConfig(
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
 ) : WebFluxConfigurer {
 
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         // Enable support for serializing protos to JSON in API responses
         configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
+        configurer.defaultCodecs().maxInMemorySize(500 * 1024 * 1024)
+
+        val partReader = DefaultPartHttpMessageReader()
+        partReader.setMaxHeadersSize(16 * 1024 * 1024)
+        partReader.isEnableLoggingRequestDetails = true
+
+        val multipartReader = MultipartHttpMessageReader(partReader)
+        multipartReader.isEnableLoggingRequestDetails = true
+
+        configurer.defaultCodecs().multipartReader(multipartReader)
     }
 }
