3fcbe7c117f13aa4576b49446bf72b57bedcf797
Cody Worsnop <cworsnop@figure.com>
2022-12-12 13:58:17 -0800

    refactoring store file (#78)
    * refactoring store file
    
    * more cleanup

--- service/src/main/kotlin/io/provenance/api/domain/usecase/objectStore/store/StoreFile.kt
+++ service/src/main/kotlin/io/provenance/api/domain/usecase/objectStore/store/StoreFile.kt
@@ -33,53 +33,65 @@ class StoreFile(
     private val entityManager: EntityManager,
 ) : AbstractUseCase<StoreFileRequestWrapper, StoreProtoResponse>() {
     override suspend fun execute(args: StoreFileRequestWrapper): StoreProtoResponse {
+        val (keyConfig, additionalAudiences, objectStoreAddress, storeRawBytes, id, file) = getParams(args.request)
+        val originator = entityManager.getEntity(KeyManagementConfigWrapper(args.uuid.toString(), keyConfig))
+
+        OsClient(URI.create(objectStoreAddress), objectStoreConfig.timeoutMs).use { osClient ->
+            val bytes = file.awaitAllBytes()
+            ByteArrayInputStream(bytes).use { message ->
+                return objectStore.store(
+                    osClient,
+                    if (!storeRawBytes)
+                        AssetOuterClassBuilders.Asset {
+                            idBuilder.value = id
+                            type = FileNFT.ASSET_TYPE
+                            description = file.filename()
+                            putKv(FileNFT.KEY_FILENAME, file.filename().toProtoAny())
+                            putKv(FileNFT.KEY_BYTES, bytes.toProtoAny())
+                            putKv(FileNFT.KEY_SIZE, bytes.size.toString().toProtoAny())
+                            putKv(FileNFT.KEY_CONTENT_TYPE, file.headers().contentType.toString().toProtoAny())
+                        } else message,
+                    originator.encryptionPublicKey() as PublicKey,
+                    additionalAudiences.map { it.encryptionKey.toJavaPublicKey() }.toSet()
+                )
+            }
+        }
+    }
+
+    private fun getParams(request: Map<String, Part>): Args {
         var additionalAudiences = emptySet<AudienceKeyPair>()
         var keyConfig: KeyManagementConfig? = null
 
-        args.request["account"]?.let {
+        request["account"]?.let {
             keyConfig = Gson().fromJson((it as FormFieldPart).value(), AccountInfo::class.java).keyManagementConfig
         }
 
-        if (!args.request.containsKey("id") || args.request.getAsType<FormFieldPart>("id").value().isEmpty()) {
+        if (!request.containsKey("id") || request.getAsType<FormFieldPart>("id").value().isEmpty()) {
             throw IllegalArgumentException("Request must provide the 'id' field for the file")
         }
 
-        args.request["permissions"]?.let {
+        request["permissions"]?.let {
             val permissions = Gson().fromJson((it as FormFieldPart).value(), PermissionInfo::class.java)
             additionalAudiences = entityManager.hydrateKeys(permissions)
         }
 
-        val originator = entityManager.getEntity(KeyManagementConfigWrapper(args.uuid.toString(), keyConfig))
-        val file = args.request.getAsType<FilePart>("file")
-        var message: Any = ByteArrayInputStream(file.awaitAllBytes())
-
-        OsClient(
-            URI.create(args.request.getAsType<FormFieldPart>("objectStoreAddress").value()),
-            objectStoreConfig.timeoutMs,
-        ).use { osClient ->
-            if (!args.request.getAsType<FormFieldPart>("storeRawBytes").value().toBoolean()) {
-                message = AssetOuterClassBuilders.Asset {
-                    idBuilder.value = args.request.getAsType<FormFieldPart>("id").value()
-                    type = FileNFT.ASSET_TYPE
-                    description = file.filename()
-
-                    putKv(FileNFT.KEY_FILENAME, file.filename().toProtoAny())
-                    putKv(FileNFT.KEY_BYTES, file.awaitAllBytes().toProtoAny())
-                    putKv(FileNFT.KEY_SIZE, file.awaitAllBytes().size.toString().toProtoAny())
-                    putKv(FileNFT.KEY_CONTENT_TYPE, file.headers().contentType.toString().toProtoAny())
-                }
-            }
-
-            return objectStore.store(
-                osClient,
-                message,
-                originator.encryptionPublicKey() as PublicKey,
-                additionalAudiences.map { it.encryptionKey.toJavaPublicKey() }.toSet()
-            )
-        }
+        val objectStoreAddress = request.getAsType<FormFieldPart>("objectStoreAddress").value()
+        val storeRawBytes = request.getAsType<FormFieldPart>("storeRawBytes").value().toBoolean()
+        val id = request.getAsType<FormFieldPart>("id").value()
+        val file = request.getAsType<FilePart>("file")
+        return Args(keyConfig, additionalAudiences, objectStoreAddress, storeRawBytes, id, file)
     }
 
     private inline fun <reified T> Map<String, Part>.getAsType(key: String): T =
         T::class.java.cast(get(key))
             ?: throw IllegalArgumentException("Failed to retrieve and cast provided argument.")
+
+    data class Args(
+        val keyConfig: KeyManagementConfig?,
+        val additionalAudiences: Set<AudienceKeyPair>,
+        val objectStoreAddress: String,
+        val storeRawBytes: Boolean,
+        val id: String,
+        val file: FilePart,
+    )
 }
