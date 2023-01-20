2f6b2c4d246d57db725b8f7c29ac41001c42db88
dkneisly-figure <dkneisly@figure.com>
2023-01-20 07:42:24 -0800

    adding alias to renamed property from pr #87

--- models/src/main/kotlin/io/provenance/api/models/cee/execute/ExecuteContractBatchRequest.kt
+++ models/src/main/kotlin/io/provenance/api/models/cee/execute/ExecuteContractBatchRequest.kt
@@ -1,11 +1,13 @@
 package io.provenance.api.models.cee.execute
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import io.provenance.api.models.account.Participant
 import io.provenance.api.models.p8e.PermissionInfo
 
 data class ExecuteContractBatchRequest(
     val config: ExecuteContractConfig,
     val records: Map<String, Any>,
+    @JsonAlias("participants")
     val additionalParticipants: List<Participant> = emptyList(),
     val permissions: PermissionInfo?,
     val chunkSize: Int = 25,
