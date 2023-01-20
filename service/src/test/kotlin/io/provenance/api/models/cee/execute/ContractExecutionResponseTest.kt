dbfb1b3aa8e8b0c85bffd76868a40e43e515e8ad
Richard Romanowski <79664656+rromanowski-figure@users.noreply.github.com>
2023-01-20 11:00:16 -0500

    add test for de/serializing sealed classes

---
+++ service/src/test/kotlin/io/provenance/api/models/cee/execute/ContractExecutionResponseTest.kt
@@ -0,0 +1,61 @@
+package io.provenance.api.models.cee.execute
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.provenance.api.models.p8e.TxResponse
+import io.provenance.scope.encryption.domain.inputstream.DIMEInputStream.Companion.configureProvenance
+import java.util.UUID
+
+class ContractExecutionResponseTest : FunSpec({
+    context("mapping") {
+        val mapper = ObjectMapper().configureProvenance()
+
+        test("SinglePartyContractExecutionResponse") {
+            val response = SinglePartyContractExecutionResponse(
+                type = "single",
+                metadata = TxResponse(
+                    hash = "hash",
+                    gasWanted = "2",
+                    gasUsed = "3",
+                    height = "4"
+                ),
+                error = "error",
+                scopeUuids = listOf(UUID.randomUUID())
+            )
+
+            val serialized = mapper.writeValueAsString(response)
+            val deserialized: ContractExecutionResponse =
+                mapper.readValue(serialized, ContractExecutionResponse::class.java)
+            (deserialized as SinglePartyContractExecutionResponse) shouldBe response
+        }
+
+        test("ContractExecutionErrorResponse") {
+            val response = ContractExecutionErrorResponse(
+                type = "single",
+                errorType = "error type",
+                error = "error",
+                scopeUuids = listOf(UUID.randomUUID(), UUID.randomUUID())
+            )
+
+            val serialized = mapper.writeValueAsString(response)
+            val deserialized: ContractExecutionResponse =
+                mapper.readValue(serialized, ContractExecutionResponse::class.java)
+            (deserialized as ContractExecutionErrorResponse) shouldBe response
+        }
+
+        test("MultipartyContractExecutionResponse") {
+            val response = MultipartyContractExecutionResponse(
+                type = "multi",
+                envelopeState = "envelope",
+                error = null,
+                scopeUuids = emptyList()
+            )
+
+            val serialized = mapper.writeValueAsString(response)
+            val deserialized: ContractExecutionResponse =
+                mapper.readValue(serialized, ContractExecutionResponse::class.java)
+            (deserialized as MultipartyContractExecutionResponse) shouldBe response
+        }
+    }
+})
