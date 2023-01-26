ec53755e9b08294a19d49d7eafad6111dd147f49
Cody Worsnop <cworsnop@figure.com>
2023-01-26 07:46:14 -0800

    cleanup

--- models/src/main/kotlin/io/provenance/api/models/p8e/ProvenanceConfig.kt
+++ models/src/main/kotlin/io/provenance/api/models/p8e/ProvenanceConfig.kt
@@ -5,6 +5,6 @@ import cosmos.tx.v1beta1.ServiceOuterClass
 data class ProvenanceConfig(
     val chainId: String,
     val nodeEndpoint: String,
-    val gasAdjustment: Double? = 1.2,
+    val gasAdjustment: Double? = 1.5,
     val broadcastMode: ServiceOuterClass.BroadcastMode = ServiceOuterClass.BroadcastMode.BROADCAST_MODE_BLOCK
 )
