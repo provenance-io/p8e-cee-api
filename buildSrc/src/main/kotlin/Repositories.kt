cf0cae44fdf0ffcea7b36541b23cdb5b688d78fe
Divya Pentela <dpentela@figure.com>
2026-08-05 11:25:24 -0700

    chore[sc-534571]: migrate Sonatype publishing to Central Portal staging API

--- buildSrc/src/main/kotlin/Repositories.kt
+++ buildSrc/src/main/kotlin/Repositories.kt
@@ -1,5 +1,5 @@
 object RepositoryLocations {
     const val JitPack = "https://javadoc.jitpack.io"
-    const val Sonatype = "https://s01.oss.sonatype.org/service/local/"
-    const val SonatypeSnapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    const val Sonatype = "https://ossrh-staging-api.central.sonatype.com/service/local/"
+    const val SonatypeSnapshot = "https://central.sonatype.com/repository/maven-snapshots/"
 }
