1532087e7ab327cdc20cda4792ecfc89aff07122
Ravi Patel <82118011+rpatel-figure@users.noreply.github.com>
2026-09-08 13:35:52 -0500

    Remove sweep job

--- service/src/main/kotlin/io/provenance/api/frameworks/config/AppConfig.kt
+++ service/src/main/kotlin/io/provenance/api/frameworks/config/AppConfig.kt
@@ -6,10 +6,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
-@EnableScheduling
 @EnableConfigurationProperties(
     value = [
         ServiceProps::class,
